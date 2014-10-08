61f6eecb4fdc5602c33e14f2c428ae89bdb5fa02
idsc3101 <idsc3101@gmail.com>
2014-10-08 17:13:12 -0500

    FInished wd7

--- DoWhileDemo/DoWhileDemo/Module1.vb
+++ DoWhileDemo/DoWhileDemo/Module1.vb
@@ -4,11 +4,18 @@ Module Module1
 
     Sub Main()
         Dim num As Integer = 1
+        Dim password As String = ""
 
         Do While num <= 7
+            num = 10
             Console.WriteLine(" " & num)
             num = num + 1
         Loop
+
+        Do
+            password = InputBox("What is the password?")
+            password = password.ToUpper
+        Loop Until password = "SHAZAM"
 
     End Sub
 
--- Millionaire/Millionaire/Form1.vb
+++ Millionaire/Millionaire/Form1.vb
@@ -11,14 +11,21 @@ Public Class Form1
         End If
 
         ' Convert into a number 
-        Dim balance As Double, numYears As Integer
+        Dim balance As Double, numYears As Integer, r As Double = 0.06
         balance = CDbl(amountSt)
 
         ' Compute the time it takes to reach the balance of a million
-        Do While balance < 1000000
-            balance += 0.06 * balance
+        Do
+            balance += r * balance
             numYears += 1
-        Loop
+            If numYears <= 10 Then
+                r += 0.005
+            End If
+            If numYears > 10 And r > 0.01 Then
+                r -= 0.005
+            End If
+
+        Loop Until balance > 1000000
 
         txtWhen.Text = "In " & numYears & " years you will have a million dollars."
     End Sub
